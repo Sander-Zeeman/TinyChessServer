d454358a48c3d539c5234ec4fc7534e42a6967cd
Sander-Zeeman <49650566+Sander-Zeeman@users.noreply.github.com>
2020-12-31 00:02:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const port = process.env.PORT || 5000
 const app = express()
+app.use(express.json())
 
-app.get('/api', (req, res) => {
-  res.status(200).json({api: 'version 1'})
+const getInfo = (request) => {
+    return request.x
+}
+
+const startGame = (info) => {
+    console.log(info)
+}
+
+app.post('/newGame', (req, res) => {
+  const info = getInfo(req.body);
+  res.json({data: info});
+  startGame(info)
 })
 
 app.listen(port, () => console.log('Server started at port', port))
